b74f7835d8914d0a31e4d18d369cdd9972e23054
networkpenetrationtester <77405346+networkpenetrationtester@users.noreply.github.com>
2026-08-13 04:57:56 -0500

    fix: simplify event loop, no longer closes stdio feat+refactor: add new validator for timestamp ranges (was in prompt)

--- src/prompter.ts
+++ src/prompter.ts
@@ -1,57 +1,68 @@
 import { stdin, stdout } from "node:process";
 import { createInterface } from "node:readline";
 
+export const io = createInterface(stdin, stdout);
+
+export async function AsyncPrompt(prompt: string) {
+  io.setPrompt(prompt);
+  io.prompt();
+
+  return await new Promise<string>((resolve) => {
+    const listener = (input: string) => {
+      io.removeListener("line", listener);
+      resolve(input);
+    };
+
+    io.addListener("line", listener);
+  });
+}
+
+export async function AsyncConfirm(subject: string) {
+  for (
+    let confirm = "";
+    ;
+    confirm = await AsyncPrompt(`* [AsyncConfirm] Confirm ${subject}? [y/n]: `)
+  ) {
+    switch (confirm) {
+      case "y": {
+        console.log(`* [AsyncConfirm] Proceeding...`);
+        return true;
+      }
+      case "n": {
+        console.log(`* [AsyncConfirm] Aborted.`);
+        return false;
+      }
+    }
+  }
+}
+
 export async function Prompter(...prompt_texts: string[]) {
-  const rl = createInterface(stdin, stdout);
   const prompt_answers = [];
 
   for (let i = 0; i < prompt_texts.length; i++) {
     const prompt_text = prompt_texts[i];
+    const answer = await AsyncPrompt(prompt_text + ": ");
 
-    const promised_answer = new Promise<string>((resolve) => {
-      // these are fucking bugged what the fuck!!!!!!'t
-
-      const listener = async (input: string) => {
-        switch (input) {
-          case "!exit": {
-            process.exit(0);
-          }
-
-          case "!cancel": {
-            rl.close();
-            return await Prompter(...prompt_texts);
-          }
-
-          case "!defaults": {
-            rl.close();
-            i = prompt_texts.length;
-            break;
-          }
-
-          case "!clear": {
-            rl.close();
-            console.clear();
-            return await Prompter(...prompt_texts);
-          }
-
-          default: {
-            resolve(input);
-          }
-        }
-
-        rl.removeListener("line", listener);
-      };
-
-      rl.addListener("line", listener);
-    });
-
-    rl.setPrompt(`${prompt_text}: `);
-    rl.prompt();
-
-    prompt_answers.push(await promised_answer);
+    switch (answer) {
+      case "/exit": {
+        process.exit(0);
+      }
+      case "/clear": {
+        console.clear();
+        i = -1;
+        break;
+      }
+      case "/def": {
+        console.log("* [Prompter] Accepting Default Values...");
+        i = prompt_texts.length;
+        break;
+      }
+      default: {
+        prompt_answers.push(answer);
+        break;
+      }
+    }
   }
-
-  rl.close();
 
   return prompt_answers;
 }
