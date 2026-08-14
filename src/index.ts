4d9d059f5b661c151bce95b53ec4fc4adf4d262d
networkpenetrationtester <77405346+networkpenetrationtester@users.noreply.github.com>
2026-08-13 23:33:05 -0500

    feat: improve demo + lock stdin

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
+import { stdin, stdout } from "node:process";
+import { createInterface } from "node:readline";
 import { Prompt } from "./prompt";
+import { Delay } from "./utils";
 
-async function Main() {
+(async function Main() {
   console.clear();
 
   while (true) {
-    const options = await Prompt();
+    const io = createInterface(stdin, stdout);
+    const options = await Prompt(io);
 
+    io.close(); // prevent user keyboard input from interrupting pretty logs
     console.log();
 
     if (options) {
       console.log(options);
+      console.log();
     } else continue;
+
+    for (let i = 0; i < 5; i++) {
+      console.log("* [Main] Larping Download: ", options.url.href);
+      await Delay(1000);
+    }
 
     console.log();
   }
-}
-
-Main();
+})();
